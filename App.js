75b5eaaa8708c08811b4ccec91d60ebdae5ade8a
Weerayooth Manawanich <ohm.weerayooth@rabbitstale.com>
2021-01-16 21:25:25 +0700

    add assets

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+import PickerComponent from "./src/components/PickerComponent";
+import SliderComponent from "./src/components/SliderComponent";
+import * as Device from "expo-device";
+import ImagePicker from "expo-image-picker";
+import CalendarComponent from "./src/components/CalendarComponent";
+import AnimateProgressComponent from "./src/components/AnimateProgressComponent";
+import DropdownPickerComponent from "./src/components/DropdownPickerComponent";
+import TouchableComponent from "./src/components/TouchableComponent";
+import VictoryNativeComponent from "./src/components/VictoryNaitveComponent";
+import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
+import ProgressComponent from "./src/components/ProgressComponent";
+import DialogComponent from "./src/components/DialogComponent";
+import TabViewComponent from "./src/components/TabViewComponent";
+import WebViewComponent from "./src/components/WebViewComponent";
+import PdfReaderComponent from "./src/components/PdfReaderComponent";
+import ZoomableComponent from "./src/components/ZoomableComponent";
+import ImagesViewer from "./src/components/ImagesViewer";
+import StepIndicatorComponent from "./src/components/StepIndicatorComponent";
+import MultiSliderComponent from "./src/components/MultiSliderComponent";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ ...styles.container }}>
+      <MultiSliderComponent />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
